e906f9578232771832bc97374286dad7f801d92b
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-02-22 22:56:59 +0600

    started adding storybook

---
+++ Story.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var prompts = ["a Name", "a movie name", "a superlative"]
+var story = "Once upon a time %s watched the movie %s and he thought it was the %s movie of past two decades!"
